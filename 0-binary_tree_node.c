476dbca28e13cd31ea4d8b5bc47073cc7e5ede6c
Mwende <muthianisharlinemwende@gmail.com>
2022-08-24 13:27:55 +0300

    binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_node - creates a binary tree node
+ *@parent: parent node
+ *@value: node value
+ *Return: binary_tree_t or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+[A[C[C[C[C[C[C[A[C[C[C[C[A[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C
